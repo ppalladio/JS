ac93414ce2158e2a74a3068a97d25f3c7c318c0c
Yuxuan Peng <nine.slide@gmail.com>
2023-02-21 13:53:11 +0100

    set it back to initial state

--- script.js
+++ script.js
@@ -2,30 +2,54 @@
 
 const number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = number; //#delete later
+//#delete later
 
 const guess = document.querySelector('.guess').value;
 
 document.querySelector('.check').addEventListener('click', (_e) => {
     if (!guess) {
         document.querySelector('.message').textContent = 'No Number';
-    } else if (guess > number) {
+    }
+    //@when the guess is too high
+    else if (guess > number) {
         if (score > 1) {
             document.querySelector('.message').textContent = 'Too High';
             score--;
             document.querySelector('.score').textContent = score;
         } else {
             document.querySelector('.message').textContent = 'You Lost';
+            document.querySelector('.number').textContent = 0;
         }
-    } else if (guess < number) {
+    }
+    //:when the guess is too low
+    else if (guess < number) {
         if (score > 1) {
             document.querySelector('.message').textContent = 'Too Low';
             score--;
             document.querySelector('.score').textContent = score;
         } else {
             document.querySelector('.message').textContent = 'You Lost';
+            document.querySelector('.number').textContent = 0;
         }
-    } else {
-        document.querySelector('.message').textContent = 'Correct';
     }
+    // :correct answer
+    else {
+        document.querySelector('.message').textContent = 'Correct';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').textContent = number;
+    }
+});
+
+//@reset
+
+document.querySelector('.again').addEventListener('click', (_e) => {
+    score = 20;
+    number = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.body').style.backgroundColor = '#222';
+    document.querySelector('.score').textContent = 20;
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('number').textContent = '?';
+    document.querySelector('.guess').value = '';
 });
